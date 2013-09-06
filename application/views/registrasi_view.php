8ac127f50689ead182f6d433f93f4399efe0a4c9
Muslih <muslihzarth@gmail.com>
2013-09-06 21:19:42 +0800

    adding form templates

---
+++ application/views/registrasi_view.php
@@ -0,0 +1,98 @@
+<h2>Form Registrasi</h2>
+<table>
+	<tr>
+		<td>
+			<label for="nama">Nama lengkap dan gelar</label>
+		</td>
+		<td>
+			<input type="text" id="nama">
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<label for="tempatlahir">Tempat lahir</label>
+		</td>
+		<td>
+			<input type="text" id="tempatlahir">
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<label for="tanggallahir">Tanggal Lahir</label>
+		</td>
+		<td>
+			<input type="date" id="tanggallahir">
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<label for="gender">Jenis Kelamin</label>
+		</td>
+		<td>
+			
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<label for="telp">No Telp/HP</label>
+		</td>
+		<td>
+			<input type="number" id="telp">
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<label for="alamat">Alamat</label>
+		</td>
+		<td>
+			<input type="textarea" id="alamat">
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<label for="kabupaten">Kabupaten/kota</label>
+		</td>
+		<td>
+			<select name="kabupaten" id="kabupaten">
+				<option value=""></option>
+				<option value="Kota Banjarmasin">Kota Banjarmasin</option>
+				<option value="Kota Banjarbaru">Kota Banjarbaru</option>
+				<option value="Kabupaten Baritokuala">Kabupaten Baritokuala</option>
+				<option value="Kabupaten Banjar">Kabupaten Banjar</option>
+				<option value="Kabupaten Tapin">Kabupaten Tapin</option>
+				<option value="Kabupaten Hulu Sungai Selatan">Kabupaten Hulu Sungai Selatan</option>
+				<option value="Kabupaten Hulu Sungai Tengah">Kabupaten Hulu Sungai Tengah</option>
+				<option value="Kabupaten Hulu Sungai Utara">Kabupaten Hulu Sungai Utara</option>
+				<option value="Kabupaten Balangan">Kabupaten Balangan</option>
+				<option value="Kabupaten Tabalong">Kabupaten Tabalong</option>
+				<option value="Kabupaten Tanah Laut">Kabupaten Tanah Laut</option>
+				<option value="Kabupaten Tanah Bumbu">Kabupaten Tanah Bumbu</option>
+				<option value="Kabupaten Kotabaru">Kabupaten Kotabaru</option>
+			</select>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<label for="goldarah">Golongan Darah</label>
+		</td>
+		<td>
+			<input type="goldarah" id="goldarah">
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<label for="pendterakhir">Pendidikan Terakhir</label>
+		</td>
+		<td>
+			<input type="text" id="pendterakhir">
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<label for="pekerjaan">Pekerjaan</label>
+		</td>
+		<td>
+			<input type="text" id="pekerjaan">
+		</td>
+	</tr>
+</table>
